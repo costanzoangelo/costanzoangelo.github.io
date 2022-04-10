f753965c1dcb5122e0f7be590cdab039a13ba99b
angelo.costanzo <angelo.costanzo.gdc3@gmail.com>
2022-04-10 14:23:33 +0200

    costanzoangelo.github.io
    test

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function() {//appena la pagina si è caricata
+
+  /* ! slider menu */
+
+  const menuSlide = () => {
+    const burger = document.querySelector('.burger__menu');
+    const menu = document.querySelector('.header__menu');
+    const navLinks = document.querySelectorAll('.header__menu li');
+
+    burger.addEventListener('click', () => {
+      menu.classList.toggle('menu-active');
+
+      navLinks.forEach((link, index) => {
+        if(link.style.animation){
+          link.style.animation = ''
+        }else{
+          link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`;
+        }
+      });
+       burger.classList.toggle("toggle");
+    });
+  }
+
+  menuSlide();
+
+
+  /* ! current page marker */
+
+  const elems = Array.from( document.querySelectorAll( '.link') );//crea la lista di tutti gli elementi (li) con classe .link
+
+    elems.forEach( element => {//ciclo su tutti gli elementi del menu
+
+    //console.log(element.href + ' - ' + window.location.href);//visualizza in console i valori dell'href dell'ancora e l'url corrente
+    var a = element.getElementsByTagName('a')[0];
+    // console.log(element.getElementsByTagName('a'));
+    if(a.href === window.location.href)// se l'url corrente è uguale alla voce del menu allora aggiungo la classe .corrente
+     element.classList.add('current');
+   });
+})
